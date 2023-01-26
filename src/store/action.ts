e24ba16584ce8a6b5774c5a436f076d1a7fb8055
andrei110295@yandex.ru <andrei110295@yandex.ru>
2023-01-26 18:01:14 +0300

    Реализовал основной функционал и добавил store

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,6 +1,10 @@
-import {Task} from './../types/task';
+import { Task} from './../types/task';
 import {createAction} from '@reduxjs/toolkit';
 
 export const addTaskAction = createAction<Task>('app/addTask');
 
 export const editTaskAction = createAction<Task>('app/editTask');
+
+export const deleteTaskAction = createAction<Task>('app/deleteTask');
+
+export const getTasksFromLocalStoreAction = createAction('app/getTasksFromLocalStore');
