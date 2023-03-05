29d840c7fddd3d53d0fe3123d3a089bd6a5746ea
andrei110295@yandex.ru <andrei110295@yandex.ru>
2023-03-05 19:36:27 +0300

    Доработал механизм уведомления о дедлайне

--- src/components/task-card/task-card.tsx
+++ src/components/task-card/task-card.tsx
@@ -52,7 +52,7 @@ function TaskCard({task}: TaskCardProps): JSX.Element {
 
   return (
     <div className="task">
-      <article className={`task-card ${isOverDue ? 'task-card--attention' : ''}`}>
+      <article className={`task-card ${isOverDue && !task.isDone ? 'task-card--attention' : ''}`}>
         <p className="task-card__title">{task.title}</p>
         <div className="task-card__controls">
           <Checkbox isDone={task.isDone} handleCheckbox={handleIsDoneCheckbox} styleClass="task-card__checkbox"/>
@@ -64,7 +64,7 @@ function TaskCard({task}: TaskCardProps): JSX.Element {
             <span className="visually-hidden">Show details</span>
           </button>
         </div>
-        {isOverDue
+        {(isOverDue && !task.isDone)
         &&
         <svg className="task-card__attention-icon" xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 512 512" fill="#a81919">
           <path d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"/>
